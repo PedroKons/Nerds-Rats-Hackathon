1c7d8a5e27f74e42b1e33d51ce8dedc8cdd06eb7
Vinicius <48769407+ViniciusCoelhoNascimento@users.noreply.github.com>
2025-05-17 15:11:51 -0300

    GET: posicao do usuario no rank de clicks, rank de clicks top 10

--- app.js
+++ app.js
@@ -12,6 +12,31 @@ app.get('/', async (req, res) => {
     }
 })
 
+app.get('/user-rank-clicks/:id', async (req, res) => {
+    const { id } = req.params
+    const { data, error } = await supabase.query('SELECT * FROM metrics WHERE id = $1', [id])
+    
+    if (error) {
+        res.status(500).send(error)
+    } else {
+        res.status(200).send(data)
+    }
+})
+
+app.get('/rank-clicks', async (req, res) => {
+const { data, error } = await supabase
+    .from('metrics')
+    .select('*')
+    .order('quant_clicks', { ascending: false })
+    .limit(10)
+
+    if (error) {
+        res.status(500).send(error)
+    } else {
+        res.status(200).send(data)
+    }
+})
+
 app.listen({ port: 3000 }, (err) => {
     if (err) {
         console.error('Error starting server:', err)
